0daea72c99db35950963b2ca6bacb09c6b11af6f
Lucas Nunes <lucasnunnes40@gmail.com>
2022-09-23 14:44:27 -0300

    rotas
    .

--- nlz/src/app/app-routing.module.ts
+++ nlz/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { NlzSidebarComponent } from './nlz-componentes-fixos/nlz-sidebar/nlz-sidebar.component';
+import { TelaLoginComponent } from './tela-login/tela-login/tela-login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TelaLoginComponent },
+  { path: 'sidebar', component: NlzSidebarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
